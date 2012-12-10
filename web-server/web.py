2fbf45fddfa76fc5de67de5706d414cd1b8087cd
Pankaj Singh <psjinx@gmail.com>
2012-12-11 01:06:55 +0530

    added pagination on search page

--- web-server/web.py
+++ web-server/web.py
@@ -1,11 +1,11 @@
 from flask import Flask, render_template, request
-app = Flask(__name__)
-
 from pyelasticsearch import ElasticSearch
 import json
-
 from datetime import datetime
 
+from flask.ext.paginate import Pagination
+
+app = Flask(__name__)
 es = ElasticSearch("http://localhost:9200/")
 
 @app.route("/")
@@ -14,19 +14,47 @@ def home():
 
 @app.route("/search/", methods=["GET"])
 def search():
+    search = True
     _started_at = datetime.now()
     query = request.args.get("q")
     start = request.args.get("start")
     limit = request.args.get("limit")
+    page = request.args.get("page")
     
     if query:
-        start = 0 if not start else start
-        limit = 10 if not limit else limit
-        res = es.search("name:%s" % (query), index="flipkart", es_from = start, es_size = limit)
+        start = 0 if not start else int(start)
+        limit = 10 if not limit else int(limit)
+        if page:
+            page = int(page)
+            start = (page-1)*limit
+
+        d = {
+                "sort": {
+                    "ratingCount": {"order": "desc"},
+                    "ratingValue": {"order": "desc"},
+                },
+                "query": {
+                    "term": {
+                        "name": query.lower(),
+                     },
+                }
+            }
+
+        #return json.dumps(d)
+        res = es.search(d, index="flipkart", es_from = start, es_size = limit)
+        total = res["hits"]["total"]
+        pagination = Pagination(found = total, search = search, total=total, per_page = limit)
+
         #return json.dumps(res)
         products = res["hits"]["hits"]
         _ended_at = datetime.now()
-        return render_template("search.html", products=products, time_taken=(_ended_at - _started_at).total_seconds())
+        return render_template("search.html",
+                               products=products,
+                               time_taken=(_ended_at - _started_at).total_seconds(),
+                               pagination=pagination,
+                               start = start,
+                               count = total
+                               )
     return "Query can't be empty."
 
 if __name__ == "__main__":
