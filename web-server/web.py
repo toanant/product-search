36ce8132b1594dd9f22e935d485f0fd7ec22f51b
Pankaj Singh <psjinx@gmail.com>
2012-12-10 22:57:08 +0530

    improved search page

--- web-server/web.py
+++ web-server/web.py
@@ -1,9 +1,10 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 app = Flask(__name__)
 
 from pyelasticsearch import ElasticSearch
-
 import json
+
+from datetime import datetime
 
 es = ElasticSearch("http://localhost:9200/")
 
@@ -11,15 +12,21 @@ es = ElasticSearch("http://localhost:9200/")
 def home():
     return render_template("home.html")
 
-@app.route("/search/<query>")
-@app.route("/search/<query>/<limit>")
-@app.route("/search/<query>/<start>/<limit>")
-def search(query=None, start = 0, limit = 50):
+@app.route("/search/", methods=["GET"])
+def search():
+    _started_at = datetime.now()
+    query = request.args.get("q")
+    start = request.args.get("start")
+    limit = request.args.get("limit")
+    
     if query:
+        start = 0 if not start else start
+        limit = 10 if not limit else limit
         res = es.search("name:%s" % (query), index="flipkart", es_from = start, es_size = limit)
         #return json.dumps(res)
         products = res["hits"]["hits"]
-        return render_template("search.html", products=products)
+        _ended_at = datetime.now()
+        return render_template("search.html", products=products, time_taken=(_ended_at - _started_at).total_seconds())
     return "Query can't be empty."
 
 if __name__ == "__main__":
